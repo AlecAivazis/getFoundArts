bf7e82284e7114ac33eda71d374db9c3ef31cf41
alec aivazis <alec@aivazis.com>
2015-09-24 18:52:56 -0700

    added splash page text

--- src/apps/frontend/views/splash.js
+++ src/apps/frontend/views/splash.js
@@ -1,6 +1,8 @@
 // third party imports
 import React from 'react'
 import Radium from 'radium'
+// local imports
+import SplashCategory from '../components/splashCategory'
 
 
 @Radium
@@ -10,6 +12,43 @@ class Splash extends React.Component {
 
 
     static defaultProps = {}
+
+    categories = [
+        {
+            header: 'Get Seen: Visualize Your Music',
+            text: 'Connect with visual artists to build your band’s visual aesthetic and create',
+            bullets: [
+                'Album Art (CD/LP/mp3 covers)',
+                'Tour Merchandise (t-shirt, poster, bag, button, etc.)',
+                'Band Photography (Camera)',
+                'Music Videos (Video Camera)',
+                'Logo Design (Blank Logo)',
+
+            ],
+            image: 'bar',
+        },
+        {
+            header: 'Get Heard: Authenticate Your Brand',
+            text: 'With cohesive visual identification and branding you will:',
+            bullets: [
+                'Sell more records and merchandise',
+                'Generate more plays and downloads (Spotify, Soundcloud, iTunes)',
+                'Gain an audience in the video field with attention-grabbing music videos (YouTube, Vimeo)',
+                'Bring more viewers to your website and social platforms with additional visual content and marketing opportunities',
+            ],
+            image: 'foo',
+        },
+        {
+            header: 'Get Found: Take Your Band (and Brand) Full-throttle',
+            text: '',
+            bullets: [
+                'Establish lasting relationships in the Art and Music world in both the artist and audience sphere',
+                'Team with other local for joint exhibitions in art galleries and concert venues. ',
+                'Relax knowing a professional, like-minded creative is handling your visual representation while you can devote your energy to recording and touring',
+            ]
+        }
+
+    ]
 
 
     constructor(props) {
@@ -35,7 +74,16 @@ class Splash extends React.Component {
         // render the component
         return (
             <div style={styles.container_style} {...unused_props}>
-                foo bar
+                {this.categories.map((category, index) => {
+                    return (
+                        <SplashCategory header={category.header}
+                                           text={category.text}
+                                           bullets={category.bullets}
+                                           image={category.image}
+                                           index={index}
+                                           key={index} />
+                    )
+                })}
             </div>
         )
     }
