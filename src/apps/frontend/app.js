8513c9944328d989121425445ff2d14d18736766
Monte Mishkin <monte@mishkin.com>
2015-10-18 18:01:06 -0700

    whoops, should have been in last commit :/

---
+++ src/apps/frontend/app.js
@@ -0,0 +1,62 @@
+// third party imports
+import express from 'express'
+import React from 'react'
+import {renderToString} from 'react-dom/server'
+import {RoutingContext, match} from 'react-router'
+import {Provider} from 'react-redux'
+// local imports
+import {templatesDir} from 'config/projectPaths'
+import routes from './routes'
+import store from './store'
+
+
+// create the express app
+const app = express()
+
+
+// use jade as the templating engine
+app.set('view engine', 'jade')
+app.set('views', templatesDir)
+
+
+// any url that hits this app
+app.all('*',
+    (req, res) => {
+        // figure out the appropriate route
+        match({routes, location: req.url}, (error, redirectLocation, renderProps) => {
+            // if there was an error
+            if (error) {
+                res.status(500).send(error.message)
+            // if the location is a redirect
+            } else if (redirectLocation) {
+                res.redirect(302, redirectLocation.pathname + redirectLocation.search)
+            // if the location was found and is not a redirect
+            } else if (renderProps) {
+                // initial application state
+                const initialState = JSON.stringify(store.getState())
+                // initial component to render
+                const initialComponent = (
+                    <Provider store={store}>
+                        <RoutingContext {...renderProps} />
+                    </Provider>
+                )
+
+                // render the jade template with the component mounted
+                res.render('index.jade', {
+                    initialState,
+                    renderedComponent: renderToString(initialComponent),
+                })
+            // otherwise the location was not found
+            } else {
+                res.status(404).send('Not found')
+            }
+        })
+    }
+)
+
+
+// export the application
+export default app
+
+
+// end of file
